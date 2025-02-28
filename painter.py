515514edeb4b7759606e70686872c37f0abddd28
manpaco <116981639+manpaco@users.noreply.github.com>
2025-02-28 15:57:35 -0300

    checking arguments and canvas overflow

--- painter.py
+++ painter.py
@@ -9,6 +9,7 @@ from PIL import Image
 # OpenBased Canvas IPv6 address
 # canvas.openbased.com
 baseip = '2602:f75c:c0::'
+max = 65535
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Draw an image by sending ICMP packets (AKA pings) to an IPv6 address')
@@ -19,6 +20,14 @@ parser.add_argument('-d', '--delay', type=float, default=1, help='the delay betw
 parser.add_argument('-b', '--baseip', default=baseip, help=f'the base IPv6 address to draw to. format: {{BASEIP}}XXXX:YYYY:RRGG:BBAA. default: {baseip} (str)')
 parser.add_argument('--verbose', action='store_true', help='print the ping command before executing')
 args = parser.parse_args()
+
+if args.delay < 0:
+    print('Error: delay must be greater than or equal to 0')
+    sys.exit(1)
+
+if args.x < 0 or args.y < 0:
+    print('Error: x and y must be greater than or equal to 0')
+    sys.exit(1)
 
 # Open the image
 try:
@@ -33,6 +42,14 @@ img = img.convert('RGBA')
 # Size of the image
 width, height = img.size
 pixels = width * height
+
+if args.x + width > max or args.y + height > max:
+    print('Error: you are trying to draw outside the canvas')
+    if args.x + width > max:
+        print(f'Suggested x: {max - width}')
+    if args.y + height > max:
+        print(f'Suggested y: {max - height}')
+    sys.exit(1)
 
 drawn = 0
 # Draw the image
